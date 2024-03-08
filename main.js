7b5a5994d37e420a2cb97ab8d5cff552f5a77549
Name <you@example.com>
2024-03-08 21:45:10 +0200

    day 1

---
+++ main.js
@@ -0,0 +1,58 @@
+h1 = document.querySelector("h1")
+
+h1.style.color = "red"
+h1.style.fontSize = "80px"
+h1.style.fontWeight = "bold"
+h1.style.textAlign = "center"
+h1.style.fontFamily = "Arial"
+
+// window.alert(10 + 20)
+// window.alert("10 + 20")
+
+// document.write("<h2>Elzero</h2>")
+// console.log("Hello From JS File")
+
+// console.error("error massage")
+// console.error( 50 + 50 )
+
+// osama = "osama"
+
+// console.table( [1, 2, 3, 4 ])
+// console.table( ["1", "2", "3", "4"] )
+// console.table( [osama, "mohamed", "yousef", "ayman"] )
+
+// console.log("%cElzero %cweb %cschool", "color: red; font-size: 40px;", "color: green; foont-size:boldl;  font-size: 40px;", "color: blue; font-size: 40px;" )
+
+// console.group("Group 1")
+// console.log("Massage One")
+// console.log("Massage Two")
+// console.group("Child Group")
+// console.log("Massage One")
+// console.log("Massage Two")
+// console.group("Grand Child Group")
+// console.log("Massage One")
+// console.log("Massage Two")
+// console.groupEnd()
+// console.groupEnd()
+// console.groupEnd()
+// console.group("Group 2")
+// console.log("Massage One")
+// console.log("Massage Two")
+// console.groupEnd()
+
+
+// console.table([ "Elzero", "Ahmad", "Samah", "Gamal", "Aya"])
+
+// console.log("Osama Mohamed")    //string
+// console.log(typeof ("Osama Mohamed"))   //string
+// console.log(typeof "Osama Mohamed")     //string
+// console.log(typeof 5000)    //number
+// console.log(typeof 5000.99) //number
+// console.log(typeof [1, 2, 3, 4, 5])     //array => object
+// console.log(typeof { name: 'osama', age: 30, state: 'married' })    //object
+// console.log(typeof true)    //boolean
+// console.log(typeof false)   //boolean
+// console.log(typeof undefined)   //undefined
+// console.log(typeof null)    //object
+
+
